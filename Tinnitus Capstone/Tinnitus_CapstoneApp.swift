83edbd1c94796c291e38088f2a07d89f8fba822b
VasylShevtsov <101153960+VasylShevtsov@users.noreply.github.com>
2025-10-04 20:21:08 -0700

    Initial Commit

---
+++ Tinnitus Capstone/Tinnitus_CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinnitus_CapstoneApp.swift
+//  Tinnitus Capstone
+//
+//  Created by Basil Shevtsov on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Tinnitus_CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
