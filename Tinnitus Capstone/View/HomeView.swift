aa2ecac4ee35960daffc8514ea84612ebb821db2
Stransl2425 <66276910+Stransl2425@users.noreply.github.com>
2025-11-20 15:39:58 -0800

    Created the UI for the LoginView

--- Tinnitus Capstone/View/HomeView.swift
+++ Tinnitus Capstone/View/HomeView.swift
@@ -5,6 +5,12 @@
 //  Created by Anika Patel on 11/10/25.
 //
 
+import SwiftUI 
+
 struct HomeView: View {
+    
+    var body: some View {
+            Text("Home View")
+        }
     
 }
--- Tinnitus Capstone/View/SignUpOrLoginView.swift
+++ Tinnitus Capstone/View/SignUpOrLoginView.swift
@@ -5,6 +5,18 @@
 //  Created by Anika Patel on 11/10/25.
 //
 
-struct SignUpOrLogin: View {
-    
+import SwiftUI
+
+struct SignUpOrLoginView: View {
+    @Binding var isLoggedIn: Bool   // <-- ADD THIS
+
+        var body: some View {
+            VStack {
+                Text("Sign Up or Log in View")
+
+                Button("Log In") {
+                    isLoggedIn = false   // example action
+                }
+            }
+        }
 }
