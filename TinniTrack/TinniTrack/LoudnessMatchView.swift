6bd6fd186941d458bbec0797c725050196663727
Vasyl Shevtsov <101153960+VasylShevtsov@users.noreply.github.com>
2025-12-04 17:44:42 -0800

    more changes
    I forgot to stage these last commit

--- TinniTrack/TinniTrack/LoudnessMatchView.swift
+++ TinniTrack/TinniTrack/LoudnessMatchView.swift
@@ -5,54 +5,6 @@
 //  Created by Anika Patel on 11/10/25.
 //
 
-/*
-import SwiftUI
-
-struct LoudnessMatchView: View {
-    @State private var loudness: Double = 0.5 //creating a variable for Loudness
-    
-    var body: some View {
-        
-        //MENU ICON
-        VStack (alignment: .leading, spacing: 24){
-            HStack { //this makes a horizontal stack from L to R
-                Image(systemName: "line.3.horizontal") //menu icon
-                    .font(.title3) // icon size- making larger
-                Spacer() //pushes everything to the left
-                
-            }
-            .padding(.horizontal) //adds L & R padding so it is not stuck to side
-            .padding(.top) //^^ but from the top
-            
-            //TITLE
-            Text("LOUDNESS MATCHING")
-                .font(.system(size:24, weight: .bold))
-                //.padding(.horizontal)
-                .frame(maxWidth: .infinity, alignment: .center)
-            
-            Spacer()
-                
-            //SLIDER
-                
-            Slider(value: $loudness, in: 0...1) //creates standard horizontal slider, this ties value from slider to var loudness
-                .rotationEffect(.degrees(-90)) //rotates to make vertical
-                .frame(height: 320) //height of slider
-                .padding(.leading, 20)
-            Spacer()
-        }
-        //BACKGROUND
-        .background(Color.white.ignoresSafeArea()) //sets background to white
-        
-    }
-    
-
-}
-
-#Preview{
-    LoudnessMatchView()
-}
-*/
-
 import SwiftUI
 
 struct LoudnessMatchView: View {
--- Tinnitus_Capstone_Legacy/Tinnitus Capstone/App/TinnitusAppStandard.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-class TinnitusAppStandard {
-    // This class manages the data flow in the application.
-    // Spezi dependencies removed.
-}
